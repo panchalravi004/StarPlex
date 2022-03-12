f4751ed93c933a1b4400a779b25edf9c9d47d2b9
Ravi Panchal <panchalravi018@gmail.com>
2022-03-12 18:14:36 +0530

    NavBar Completed

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+//load all file in main index file
+$(document).ready(
+    function () {
+        $("#header").load("header/header.html");
+        // $("#footer").load("footer/footer.html");
+        // $("#offer-container").load("offers/offer.html");
+        // $("#product").load("products/product.html");
+    }
+);
